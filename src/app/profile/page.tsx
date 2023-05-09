9d5f3d0f50acfc84c6be44e150529bcdb4b85260
ASkelly2001 <x00161363@myTUDublin.ie>
2023-05-10 00:24:08 +0100

    moved delete user code to function

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,23 +2,14 @@
 
 import { auth } from '../page';
 import { useAuthState, useDeleteUser } from 'react-firebase-hooks/auth';
-import { DeleteAccountButton } from '@/functions/handleDeleteUser';
-
-async function handleDeleteAccount() {
-    try {
-        await auth.currentUser?.delete();
-        console.log("User account deleted successfully!");
-    } catch (error) {
-        console.error("Error deleting user account: ", error);
-    }
-}
+import { handleDeleteUser } from '@/functions/handleDeleteUser';
 
 export default function Profile() {
 
     return (
         <>
             <p>ho</p>
-            <button onClick={handleDeleteAccount}>
+            <button onClick={handleDeleteUser}>
                 Delete Account
             </button>
         </>
--- src/functions/handleDeleteUser.tsx
+++ src/functions/handleDeleteUser.tsx
@@ -1,23 +1,12 @@
 import { auth } from "@/app/page";
-import { getAuth, deleteUser } from "firebase/auth";
-const DeleteAccountButton = () => {
-    const handleDeleteAccount = async () => {
-        try {
-            await auth.currentUser?.delete();
-            console.log("User account deleted successfully!");
-        } catch (error) {
-            console.error("Error deleting user account: ", error);
-        }
-    };
 
-    return (
-        <>
-        <p>ho</p>
-        <button onClick={handleDeleteAccount}>
-            Delete Account
-        </button>
-        </>
-    );
-};
+async function handleDeleteUser() {
+    try {
+        await auth.currentUser?.delete();
+        console.log("User account deleted successfully!");
+    } catch (error) {
+        console.error("Error deleting user account: ", error);
+    }
+}
 
-export { DeleteAccountButton }
+export { handleDeleteUser }
